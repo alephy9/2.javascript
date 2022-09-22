88471ce8615b05ce2f308d699bb4472f3db1bf04
alephy9 <aleephy@hotmail.com>
2022-09-22 09:54:51 -0300

    js

---
+++ 1.javascript-teoria/003/desvio-condicional.js
@@ -0,0 +1,9 @@
+var velocidade = 60
+
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade > 50) {
+console.log(`Você foi mutado`)
+}
+
+console.log(`Dirija sempre usando cinto!`)
