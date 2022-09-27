b9638734e2e6ba4825ad236fd93a9fd5606e9f0c
Alephy <106988075+alephy9@users.noreply.github.com>
2022-09-27 17:28:57 -0300

    Create 2.pratica.js

---
+++ 2.javascript-pratica/2.pratica.js
@@ -0,0 +1,15 @@
+
+// Quero saber se o aluno foi aprovado, com média 6,0
+
+var nota1 = 4
+var nota2 = 6
+var nota3 = 7
+var nota4 = 6
+var notafinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+if (notafinal >= 6) {
+    console.log(`Aprovado com a média: ${notafinal}`)
+} else {
+    console.log(`Reprovado com a média: ${notafinal}`)
+}
+
